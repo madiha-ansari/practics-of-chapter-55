c947335edf6997901c0a77556a8c85b070ae94f1
madiha-ansari <157696198+madiha-ansari@users.noreply.github.com>
2024-09-28 08:43:41 +0500

    whatspp read more

---
+++ app.js
@@ -0,0 +1,9 @@
+    function showText(){
+        var text = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui illum officiis quae quos ipsum officia facere eum a,
+    beatae consequatur cumque asperiores ad dignissimos.Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui illum officiis quae quos ipsum officia facere eum a,
+    beatae consequatur cumque asperiores ad dignissimos.Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui illum officiis quae quos ipsum officia facere eum a,
+    beatae consequatur cumque asperiores ad dignissimos.Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui illum officiis quae quos ipsum officia facere eum a,
+    beatae consequatur cumque asperiores ad dignissimos.Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui illum officiis quae quos ipsum officia facere eum a,
+    beatae consequatur cumque asperiores ad dignissimos.`
+   document.getElementById("aliHuzzi").innerHTML = text;
+}
